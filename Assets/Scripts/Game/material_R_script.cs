6f56dfcf090bc49c5f1b37fd094b916988ba6782
itam14 <61597755+itam14@users.noreply.github.com>
2020-02-28 23:05:18 +0100

    add material script for "R"

---
+++ Assets/Scripts/Game/material_R_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class material_R_script : MonoBehaviour
+{
+    public Material matR0;
+    public Material matR1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Invoke("m1", 1.0f);
+
+    }
+
+    void m1()
+    {
+        GetComponent<Renderer>().material = matR0;
+
+        Invoke("m2", 1.0f);
+
+    }
+
+    void m2()
+    {
+        GetComponent<Renderer>().material = matR1;
+
+        Invoke("m1", 1.0f);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
