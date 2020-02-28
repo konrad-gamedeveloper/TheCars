c976b91af20ec8f4eee08eba1110d3885e8de12a
konrad-gamedeveloper <61595841+konrad-gamedeveloper@users.noreply.github.com>
2020-02-28 22:47:49 +0100

    Checkpoint

---
+++ Assets/Scripts/Game/CheckpointScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    public CarControlScript cars;
+
+    public Vector3 fixedRotation;
+    public Vector3 fixedPosition;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        cars.lastCheckpoint_position = gameObject.transform.position;
+        cars.lastCheckpoint_rotation = fixedRotation;
+
+        cars.flaga_first = true;
+    }
+
+    private void Start()
+    {
+        cars = GameObject.Find("CAR_1").GetComponent<CarControlScript>();
+    }
+
+
+}
